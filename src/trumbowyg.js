dc562fb24aa424081ebdd5317208ae717cab71c8
JP Wrye <jwrye@air.org>
2023-10-17 16:14:42 -0400

    Created an HTML editor widget using trumbowyg.js

---
+++ src/trumbowyg.js
@@ -0,0 +1,133 @@
+function init(Survey, $) {
+    const iconId = "icon-editor";
+    const componentName = "trumbowyg";
+    Survey.SvgRegistry && Survey.SvgRegistry.registerIconFromSvg(iconId, require('svg-inline-loader!./images/editor.svg'), "");
+    $ = $ || window.$;
+    var widget = {
+        name: componentName,
+        title: "Editor",
+        iconName: iconId,
+        widgetIsLoaded: function () {
+            return typeof $.trumbowyg != "undefined";
+        },
+        isFit: function (question) {
+            return question.getType() === componentName;
+        },
+        htmlTemplate:
+            "<div></div>",
+        activatedByChanged: function (activatedBy) {
+            Survey.Serializer.addClass(componentName, [], null, "empty");
+            let registerQuestion = Survey.ElementFactory.Instance.registerCustomQuestion;
+            if (!!registerQuestion) registerQuestion(componentName);
+            Survey.Serializer.addProperty(componentName, {
+                name: "height",
+                default: 300,
+                category: "general",
+            });
+        },
+        afterRender: function (question, el) {
+            var name = question.inputId;
+            el.name = name;
+            $(el).trumbowyg('destroy');
+
+            var $editor = $(el);
+
+            MathJax.Hub.Config({
+                tex2jax: {
+                    inlineMath: [
+                        ['$', '$'],
+                        ['\\(', '\\)']
+                    ]
+                }
+            });
+
+            $editor.trumbowyg({
+                btns: [
+                    ['viewHTML'],
+                    ['undo', 'redo'], // Only supported in Blink browsers
+                    ['formatting'],
+                    ['strong', 'em', 'del'],
+                    ['superscript', 'subscript'],
+                    ['link'],
+                    ['insertImage'],
+                    ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+                    ['unorderedList', 'orderedList'],
+                    ['horizontalRule'],
+                    ['removeformat'],
+                    ['mathml'],
+                    ['fullscreen']
+                ]
+            });
+
+            if (question.isReadOnly) {
+                $editor.trumbowyg('disable');
+            } else {
+                $editor.trumbowyg('enable');
+            }
+
+            var isValueChanging = false;
+            var updateValueHandler = function () {
+
+                if (isValueChanging || typeof question.value === "undefined") {
+                    return;
+                }
+
+                $editor.trumbowyg('html', question.value);
+            };
+
+            $editor.on("tbwchange", function () {
+                isValueChanging = true;
+                question.value = $editor.trumbowyg('html');
+                isValueChanging = false;
+            });
+
+            question.valueChangedCallback = updateValueHandler;
+            question.readOnlyChangedCallback = function () {
+                if (question.isReadOnly) {
+                    $editor.trumbowyg('disable');
+                } else {
+                    $editor.trumbowyg('enable');
+                }
+            };
+            updateValueHandler();
+        },
+        willUnmount: function (question, el) {
+            question.readOnlyChangedCallback = null;
+            $(el).trumbowyg('destroy');
+            $(el).remove();
+        },
+        pdfRender: function (survey, options) {
+            if (options.question.getType() === componentName) {
+                const loc = new Survey.LocalizableString(survey, true);
+                loc.text = options.question.value || options.question.defaultValue;
+                options.question["locHtml"] = loc;
+                if (
+                    options.question.renderAs === "standard" ||
+                    options.question.renderAs === "image"
+                ) {
+                    options.question["renderAs"] = options.question.renderAs;
+                } else options.question["renderAs"] = "auto";
+                const flatHtml = options.repository.create(
+                    survey,
+                    options.question,
+                    options.controller,
+                    "html"
+                );
+                return new Promise(function (resolve) {
+                    flatHtml.generateFlats(options.point).then(function (htmlBricks) {
+                        options.bricks = htmlBricks;
+                        resolve();
+                    });
+                });
+            }
+        },
+    };
+
+    Survey.CustomWidgetCollection.Instance.addCustomWidget(widget, "customtype");
+}
+
+if (typeof Survey !== "undefined") {
+    init(Survey, window.$);
+}
+
+export default init;
--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,9 @@ const today = new Date();
 const year = today.getFullYear();
 
 var copyright = [
-  "surveyjs-widgets - Widgets for the SurveyJS library v" + packageJson.version,
-  "Copyright (c) 2015-" + year + " Devsoft Baltic OÜ  - http://surveyjs.io/",
-  "License: MIT (http://www.opensource.org/licenses/mit-license.php)"
+    "surveyjs-widgets - Widgets for the SurveyJS library v" + packageJson.version,
+    "Copyright (c) 2015-" + year + " Devsoft Baltic OÜ  - http://surveyjs.io/",
+    "License: MIT (http://www.opensource.org/licenses/mit-license.php)"
 ].join("\n");
 
 var outputFolder = "package";
@@ -23,164 +23,167 @@ var outputFolder = "package";
 var main = "surveyjs-widgets";
 
 var widgets = [
-  main,
-  "select2",
-  "select2-tagbox",
-  "jquery-ui-datepicker",
-  "inputmask",
-  "icheck",
-  "jquery-bar-rating",
-  "sortablejs",
-  "nouislider",
-  "ck-editor",
-  "easy-autocomplete",
-  "pretty-checkbox",
-  "bootstrap-slider",
-  "microphone",
-  "emotionsratings"
+    main,
+    "select2",
+    "select2-tagbox",
+    "jquery-ui-datepicker",
+    "inputmask",
+    "icheck",
+    "jquery-bar-rating",
+    "sortablejs",
+    "nouislider",
+    "ck-editor",
+    "easy-autocomplete",
+    "pretty-checkbox",
+    "bootstrap-slider",
+    "microphone",
+    "emotionsratings",
+    "trumbowyg"
 ];
 
 var dependencies = {
-  select2: "^4.0.4",
-  icheck: "^1.0.2",
-  "jquery-ui": "^1.12.1",
-  sortablejs: "^1.6.1",
-  nouislider: "^14.6.3",
-  inputmask: "^5.0.3",
-  "jquery-bar-rating": "^1.2.2",
-  "easy-autocomplete": "^1.3.5",
-  "pretty-checkbox": "^3.0.3",
-  "bootstrap-slider": "^10.0.0",
-  recordrtc: "^5.4.6",
-  "emotion-ratings": "^2.0.1"
+    select2: "^4.0.4",
+    icheck: "^1.0.2",
+    "jquery-ui": "^1.12.1",
+    sortablejs: "^1.6.1",
+    nouislider: "^14.6.3",
+    inputmask: "^5.0.3",
+    "jquery-bar-rating": "^1.2.2",
+    "easy-autocomplete": "^1.3.5",
+    "pretty-checkbox": "^3.0.3",
+    "bootstrap-slider": "^10.0.0",
+    recordrtc: "^5.4.6",
+    "emotion-ratings": "^2.0.1",
+    "trumbowyg": "^2.27.3",
+    "mathjax": "2.7.5"
 };
 
 var entry = {};
 
 module.exports = function (options) {
-  var packagePath = `./${outputFolder}`;
+    var packagePath = `./${outputFolder}`;
 
-  var targetPackageJson = {
-    name: `surveyjs-widgets`,
-    version: packageJson.version,
-    description: "Custom widgets for the SurveyJS library",
-    keywords: [
-      "Survey",
-      "JavaScript",
-      "Bootstrap",
-      "Library",
-      "SurveyJS",
-      "Widgets"
-    ],
-    homepage: "https://surveyjs.io/",
-    license: "MIT",
-    files: [],
-    main: main + ".js",
-    repository: {
-      type: "git",
-      url: "https://github.com/surveyjs/widgets.git"
-    },
-    dependencies: {
-      jquery: "^3.2.1"
-    },
-    peerDependencies: {}
-  };
+    var targetPackageJson = {
+        name: `surveyjs-widgets`,
+        version: packageJson.version,
+        description: "Custom widgets for the SurveyJS library",
+        keywords: [
+            "Survey",
+            "JavaScript",
+            "Bootstrap",
+            "Library",
+            "SurveyJS",
+            "Widgets"
+        ],
+        homepage: "https://surveyjs.io/",
+        license: "MIT",
+        files: [],
+        main: main + ".js",
+        repository: {
+            type: "git",
+            url: "https://github.com/surveyjs/widgets.git"
+        },
+        dependencies: {
+            jquery: "^3.2.1"
+        },
+        peerDependencies: {}
+    };
 
-  widgets.forEach(function (widget) {
-    if (widget !== main) {
-      targetPackageJson.files.push(`widgets/${widget}.js`);
-      targetPackageJson.files.push(`widgets/${widget}.min.js`);
-      targetPackageJson.files.push(`widgets/${widget}.min.js.map`);
-      entry["widgets/" + widget] = path.join(__dirname, `./src/${widget}.js`);
-    } else {
-      targetPackageJson.files.push(`${widget}.js`);
-      targetPackageJson.files.push(`${widget}.min.js`);
-      targetPackageJson.files.push(`${widget}.min.js.map`);
-      entry[widget] = path.join(__dirname, `./src/${widget}.js`);
+    widgets.forEach(function (widget) {
+        if (widget !== main) {
+            targetPackageJson.files.push(`widgets/${widget}.js`);
+            targetPackageJson.files.push(`widgets/${widget}.min.js`);
+            targetPackageJson.files.push(`widgets/${widget}.min.js.map`);
+            entry["widgets/" + widget] = path.join(__dirname, `./src/${widget}.js`);
+        } else {
+            targetPackageJson.files.push(`${widget}.js`);
+            targetPackageJson.files.push(`${widget}.min.js`);
+            targetPackageJson.files.push(`${widget}.min.js.map`);
+            entry[widget] = path.join(__dirname, `./src/${widget}.js`);
+        }
+    });
+
+    targetPackageJson.dependencies = Object.assign(
+        targetPackageJson.dependencies,
+        dependencies
+    );
+
+    var config = {
+        entry: entry,
+        output: {
+            path: path.join(__dirname, packagePath),
+            filename: `[name].${options.buildType === "prod" ? "min." : ""}js`,
+            library: "[name]",
+            libraryTarget: "umd",
+            umdNamedDefine: true
+        },
+        externals: {
+            jquery: {
+                root: "jQuery",
+                commonjs2: "jquery",
+                commonjs: "jquery",
+                amd: "jquery"
+            },
+            inputmask: {
+                root: "Inputmask",
+                commonjs2: "inputmask",
+                commonjs: "inputmask",
+                amd: "inputmask"
+            },
+            nouislider: {
+                root: "noUiSlider",
+                commonjs2: "nouislider",
+                commonjs: "nouislider",
+                amd: "nouislider"
+            },
+            sortablejs: {
+                root: "Sortable",
+                commonjs2: "sortablejs",
+                commonjs: "sortablejs",
+                amd: "sortablejs"
+            },
+            "bootstrap-slider": {
+                root: "Slider",
+                commonjs2: "bootstrap-slider",
+                commonjs: "bootstrap-slider",
+                amd: "bootstrap-slider"
+            }
+        },
+        optimization: {
+            minimize: options.buildType === "prod"
+        },
+        mode: options.buildType === "prod" ? "production" : "development",
+        plugins: [
+            new webpack.NoEmitOnErrorsPlugin(),
+            new FriendlyErrorsWebpackPlugin()
+        ],
+        devtool: options.buildType === "prod" ? "source-map" : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, outputFolder),
+            open: true
+        }
+    };
+
+    if (options.buildType === "dev") {
+        config.plugins = config.plugins.concat([
+            new CleanWebpackPlugin([outputFolder], { verbose: true })
+        ]);
     }
-  });
 
-  targetPackageJson.dependencies = Object.assign(
-    targetPackageJson.dependencies,
-    dependencies
-  );
-
-  var config = {
-    entry: entry,
-    output: {
-      path: path.join(__dirname, packagePath),
-      filename: `[name].${options.buildType === "prod" ? "min." : ""}js`,
-      library: "[name]",
-      libraryTarget: "umd",
-      umdNamedDefine: true
-    },
-    externals: {
-      jquery: {
-        root: "jQuery",
-        commonjs2: "jquery",
-        commonjs: "jquery",
-        amd: "jquery"
-      },
-      inputmask: {
-        root: "Inputmask",
-        commonjs2: "inputmask",
-        commonjs: "inputmask",
-        amd: "inputmask"
-      },
-      nouislider: {
-        root: "noUiSlider",
-        commonjs2: "nouislider",
-        commonjs: "nouislider",
-        amd: "nouislider"
-      },
-      sortablejs: {
-        root: "Sortable",
-        commonjs2: "sortablejs",
-        commonjs: "sortablejs",
-        amd: "sortablejs"
-      },
-      "bootstrap-slider": {
-        root: "Slider",
-        commonjs2: "bootstrap-slider",
-        commonjs: "bootstrap-slider",
-        amd: "bootstrap-slider"
-      }
-    },
-    optimization: {
-      minimize: options.buildType === "prod"
-    },
-    mode: options.buildType === "prod" ? "production" : "development",
-    plugins: [
-      new webpack.NoEmitOnErrorsPlugin(),
-      new FriendlyErrorsWebpackPlugin()
-    ],
-    devtool: options.buildType === "prod" ? "source-map" : "inline-source-map",
-    devServer: {
-      contentBase: path.join(__dirname, outputFolder),
-      open: true
+    if (options.buildType === "prod") {
+        config.plugins = config.plugins.concat([
+            new webpack.BannerPlugin(copyright),
+            new GeneratePackageJsonPlugin(targetPackageJson),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: path.join(__dirname, "./src/targetREADME.md"),
+                        to: path.join(__dirname, `${packagePath}/README.md`)
+                    }
+                ]
+            })
+        ]);
     }
-  };
 
-  if (options.buildType === "dev") {
-    config.plugins = config.plugins.concat([
-      new CleanWebpackPlugin([outputFolder], { verbose: true })
-    ]);
-  }
-
-  if (options.buildType === "prod") {
-    config.plugins = config.plugins.concat([
-      new webpack.BannerPlugin(copyright),
-      new GeneratePackageJsonPlugin(targetPackageJson),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.join(__dirname, "./src/targetREADME.md"),
-            to: path.join(__dirname, `${packagePath}/README.md`)
-          }
-        ]
-      })
-    ]);
-  }
-
-  return config;
+    return config;
 };
